537f27a74cf4956a681ec6865373b7f8a1d0583e
EmilioSarchetti <emiliosarchetti@gmail.com>
2025-06-28 18:02:03 -0300

    asd

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-
-router.get('/medicos-por-especialidad/:especialidadId', adminController.listarMedicosPorEspecialidad);
-router.get('/pacientes-atendidos/:doctorId', adminController.contarPacientesAtendidos);
-router.post('/registrar-medico', adminController.registrarMedico);
-
-module.exports = router;
--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/registro', authController.registro);
-router.post('/login', authController.login);
-
-module.exports = router;
